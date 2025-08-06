6dfc3569263a36f62e035d21e2c4a658412fcb3f
Vraiment <jemc44@gmail.com>
2025-08-06 09:39:46 -0600

    Bundler boilerplate

---
+++ spec/ruby_project_template_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyProjectTemplate do
+  it "has a version number" do
+    expect(RubyProjectTemplate::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
