371f95dfffb0217b1ec2cc538bca55ad1430a26d
Vraiment <jemc44@gmail.com>
2025-08-06 12:54:47 -0600

    Remove custom RuboCop settings

--- spec/ruby_project_template_spec.rb
+++ spec/ruby_project_template_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe RubyProjectTemplate do
-  it "has a version number" do
+  it 'has a version number' do
     expect(RubyProjectTemplate::VERSION).not_to be nil
   end
 end
